1cfaba25a669ef1519932351a7ebff32eaeb569b
Mark Hurwitz <mhurwitzus@gmail.com>
2013-12-14 17:34:52 -0600

    fixed up the wizard moving through steps

--- app/assets/javascripts/cbtApp/controllers/eventCreateController.js
+++ app/assets/javascripts/cbtApp/controllers/eventCreateController.js
@@ -1,7 +1,48 @@
 cbtApp.controller('EventCreateCtrl', ['$scope', '$http', '$stateParams', '$state', function($scope, $http, $stateParams, $state) {
 
 	console.log("current state: " + $state.current.name)
+	console.log("step: " + $state.current.data.step)
 
 	$scope.activeState = $state.current.name;
-	
+	$scope.step = $state.current.data.step
+
+	$scope.getCurrentStep = function() {
+		return $state.current.data.step;
+	}
+
+	$scope.firstStep = function(){
+		return $state.current.data.step == 0;
+	}
+
+	$scope.lastStep = function(){
+		return $state.current.data.step == 7;
+	}
+
+	$scope.goToStep = function(desired_step){
+		if(desired_step == 0){
+			$state.transitionTo('new')
+		} else if(desired_step == 1) {
+			$state.go('new.description')
+		} else if(desired_step == 2) {
+			$state.go('new.feelingsBefore')
+		} else if(desired_step == 3) {
+			$state.go('new.automaticThoughts')
+		} else if(desired_step == 4) {
+			$state.go('new.distortions')
+		} else if(desired_step == 5) {
+			$state.go('new.rationalThoughts')
+		} else if(desired_step == 6) {
+			$state.go('new.feelingsAfter')
+		} else if(desired_step == 7) {
+			$state.go('new.review')
+		}
+	};
+
+	$scope.previousStep = function(){
+		$scope.goToStep($state.current.data.step - 1)
+	}
+	$scope.nextStep = function(){
+		$scope.goToStep($state.current.data.step + 1)
+	}
+
 }])
--- app/assets/javascripts/cbtApp/router.js
+++ app/assets/javascripts/cbtApp/router.js
@@ -2,6 +2,7 @@ var cbtAppRoutes = angular.module('cbtAppRoutes', ['ui.router']);
 
 cbtAppRoutes.config(function($stateProvider, $urlRouterProvider){
 	$urlRouterProvider.when("", "/");
+	$urlRouterProvider.when("/new", "/new/instructions");
 	// For any unmatched url, send to /route1
 	$urlRouterProvider.otherwise("/");
 
@@ -17,12 +18,64 @@ cbtAppRoutes.config(function($stateProvider, $urlRouterProvider){
 		.state('new', {
 			url: '/new',
 			templateUrl: "/api/templates/new_event.html",
+			data: {
+				step: 0
+			}
+		})
+		.state('new.instructions', {
+			url: '/instructions',
+			templateUrl: "/api/templates/new.instructions.html",
+			data: {
+				step: 0
+			}
 		})
 		.state('new.description', {
 			url: '/description',
-			templateUrl: "/api/templates/create_description.html",
+			templateUrl: "/api/templates/new.description.html",
 			data: {
-				activeState: "description"
+				step: 1
+			}
+		})
+		.state('new.feelingsBefore', {
+			url: '/feelingsBefore',
+			templateUrl: "/api/templates/new.feelingsBefore.html",
+			data: {
+				step: 2
+			}
+		})
+		.state('new.automaticThoughts', {
+			url: '/automaticThoughts',
+			templateUrl: "/api/templates/new.automaticThoughts.html",
+			data: {
+				step: 3
+			}
+		})
+		.state('new.distortions', {
+			url: '/distortions',
+			templateUrl: "/api/templates/new.distortions.html",
+			data: {
+				step: 4
+			}
+		})
+		.state('new.rationalThoughts', {
+			url: '/rationalThoughts',
+			templateUrl: "/api/templates/new.rationalThoughts.html",
+			data: {
+				step: 5
+			}
+		})
+		.state('new.feelingsAfter', {
+			url: '/feelingsAfter',
+			templateUrl: "/api/templates/new.feelingsAfter.html",
+			data: {
+				step: 6
+			}
+		})
+		.state('new.review', {
+			url: '/review',
+			templateUrl: "/api/templates/new.review.html",
+			data: {
+				step: 7
 			}
 		})
 
